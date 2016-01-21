92c944f2afe92aa06e1c511d912f9b43dc4ddd64
Ben Miller <Ben Miller>
2016-01-20 21:21:13 -0700

    Add header to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append("Benjamin Miller");
+//var awesomeThoughts = "I am Ben and I am Awesome!";
+
+//var funThoughts = awesomeThoughts.replace("Awesome", "FUN");
+
+//console.log(awesomeThoughts);
+//console.log(funThoughts);
+
+//$("#main").append(funThoughts);
+
+var name = "Benjamin Miller";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Learning Experience Designer"
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
