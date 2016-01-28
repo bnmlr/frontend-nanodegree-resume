2aae6654b62dcf6231fe54f563461d8e92c11aa4
Ben Miller <Ben Miller>
2016-01-27 21:43:00 -0700

    feat: Add initial JSON objects for resume sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,12 +8,106 @@
 
 //$("#main").append(funThoughts);
 
-var name = "Benjamin Miller";
-var formattedName = HTMLheaderName.replace("%data%", name);
+//var skills = ["writing", "design", "learning theory", "front-end web dev"]; 
+//creating an array
+ //$("#main").append(skills);
+ //appending array to page
+//$("#main").append(skills[0]);
+//appends first item from array
+//var name = "Benjamin Miller";
+//var formattedName = HTMLheaderName.replace("%data%", name);
 
-var role = "Learning Experience Designer"
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+//var role = "Learning Experience Designer"
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
 
+//creating a object containing resume info
+var bio {
+	"name": "Benjamin Miller",
+	"role": "Learning Experience Designer",
+	"contacts": {
+		"mobile": "555-555-5555",
+		"email": "name@email.com",
+		"github": "bnmlr",
+		"twitter": "@benmillr",
+		"location": "Salt Lake City, UT, US"
+	},
+	"welcomeMessage": "I'm an accomplished instructional designer with experience in higher education, corporate training, and financial education.",
+	"skills": [
+		"instructional design", "content development", "front-end web development"
+	],
+	"bioPic": "images/headShot.jpg"
+};
+
+var education {
+	"schools": [
+		{
+			"name": "Utah State University",
+			"location": "Logan, UT, US",
+			"degree": "MS",
+			"major": ["Instructional Technology and Learning Sciences"],
+			"dates": "2013-2015",
+			"url": "http://www.usu.edu"
+		}, 
+		{
+			"name": "Brigham Young University",
+			"location": "Provo, UT, US",
+			"degree": "BA",
+			"major": ["English"],
+			"dates": "2005-2012",
+			"url": "http://www.byu.edu"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Intro to HTML and CSS",
+     		"school": "Udacity",
+    		"date": "12-2015",
+     		"url": "https://www.udacity.com/courses/ud304"
+		},
+		{
+			"title": "JavaScript Basics",
+     		"school": "Udacity",
+    		"date": "1-2016",
+     		"url": "https://www.udacity.com/courses/ud804"
+		}
+	]
+};
+
+var work {
+	"jobs": [
+		{
+			"employer": "Investools from TD Ameritrade",
+     		"title": "Content Manager",
+    		"location": "Salt Lake City, UT, US", 
+     		"dates": "In progress",
+     		"description": "Design and develop blended learning solutions that help people more actively manage their finances."
+		},
+		{
+			"employer": "Allen Communication Learning Services",
+     		"title": "Instructional Design Lead",
+    		"location": "Salt Lake City, UT, US", 
+     		"dates": "2012-2014",
+     		"description": "Designed and developed learning solutions to solve performance gaps."
+		}
+	]
+};
+
+var projects {
+	"projects": [
+		{
+			"title": "Portfolio Management Course",
+      		"dates": "2014-2015",
+      		"description": "An online course that teaches the basics of allocating and diversifying an investment portfolio",
+      		"images": ["images/pm.png"]
+		},
+		{
+			"title": "Competency-Based Learning Explainer Video",
+      		"dates": "2013",
+      		"description": "A video that explains competency-based learning",
+      		"images": ["images/cbe.png"]
+		}
+	]
+};
